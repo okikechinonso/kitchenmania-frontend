8c85566cf60da8f521c5b27a27ca5f8e6178e9f1
Okike Chinonso <Chinonsookike@gmail.com>
2022-01-27 20:51:23 +0100

    added sign up and sign in ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,135 @@
-
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div class="shrink-0">
-          <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+    <div class="relative bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6">
+        <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+          <div class="flex justify-start lg:w-0 lg:flex-1">
+            <link href="#">
+              <span class="sr-only">Workflow</span>
+              <img
+                class="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt=""
+              />
+            </link>
+          </div>
+          <nav class="hidden md:flex space-x-10">
+            <button
+              type="button"
+              class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              aria-expanded="false"
+            >
+              <span>Blog</span>
+            </button>
+            <button
+              type="button"
+              class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              aria-expanded="false"
+            >
+              <span>Order</span>
+            </button>
+          </nav>
+          <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <link
+              href="#"
+              class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Sign in
+            </link>
+            <link
+              href="#"
+              class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </link>
+          </div>
         </div>
-        <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-slate-500">You have a new message!</p>
+         {/* Sign up section */}
+        <div class="w-2/5 flex-col">
+          <div class="mt-1 mb-2 w-full relative rounded-md shadow-lg">
+            <input
+              type="text"
+              name="first_name"
+              id="price"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="First Name"
+            />
+          </div>
+          <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="text"
+              name="last_name"
+              id="last_name"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Last Name"
+            />
+          </div>
+          <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Email"
+            />
+          </div>
+          <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="password"
+              name="password"
+              id="price"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Password"
+            />
+          </div>
+          <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="password"
+              name="confirm_password"
+              id="price"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Confirm Password"
+            />
+          </div>
+          <link
+              href="#"
+              class="mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </link>
         </div>
+        {/* end of signup */}
+
+        {/* signin */}
+        <div class="w-2/5 flex-col">
+        <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Email"
+            />
+          </div>
+          <div class="mb-2 relative rounded-md shadow-lg">
+            <input
+              type="password"
+              name="password"
+              id="price"
+              class="focus:ring-indigo-500 focus:border-indigo-500 block w-full h-12 pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+              placeholder="Password"
+            />
+          </div>
+          <link
+              href="#"
+              class="mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign in
+            </link>
+        </div>
+        {/* end signin */}
       </div>
     </div>
   );
