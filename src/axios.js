cd315e5ba9f96bc4d87fad99ab866a1b9e778503
Okike Chinonso <Chinonsookike@gmail.com>
2022-01-30 00:43:29 +0100

    added axios url

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
-const instance = axios.create({
-  // baseURL: "https://kitchenmania-api.com/api/v1",
-  baseURL: "http://localhost:8080/api/v1",
-});
-
+let instance = null
+if (process.env.NODE_ENV !== "development"){
+  instance = axios.create({ 
+    baseURL: process.env.REACT_APP_AXIOS_URL,
+  });
+}else{
+  instance = axios.create({ 
+    baseURL: "http://localhost:8080/api/v1",
+  });
+}
 
 if (localStorage.token) {
   instance.defaults.headers.common["Authorization"] =
